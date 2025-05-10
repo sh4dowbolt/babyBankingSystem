ae0061801369762ef35b940ebdf2bcefa480bff7
sh4dowbolt <dmit.palckin@yandex.ru>
2025-05-10 15:30:44 +0400

    first commit

---
+++ src/main/java/com/suraev/babyBankingSystem/BabyBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.suraev.babyBankingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyBankingSystemApplication.class, args);
+	}
+
+}
